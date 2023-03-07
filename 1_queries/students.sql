92cef0de39733a4d12ee3895a3c157f55b2fbaca
isaac-ad <isaacadnanu@gmail.com>
2023-03-06 21:28:22 -0500

    BootcampX Assignments

---
+++ 1_queries/students.sql
@@ -0,0 +1,19 @@
+SELECT id, name 
+FROM students 
+WHERE cohort_id = 1
+ORDER BY name;
+SELECT count(id)
+FROM students 
+WHERE cohort_id IN (1,2,3);
+SELECT name, id, email, cohort_id
+FROM students
+WHERE email NOT LIKE '%gmail.com'
+AND phone IS NULL;
+SELECT name, id, cohort_id
+FROM students
+WHERE end_date IS NULL
+ORDER BY cohort_id;
+SELECT name, email, phone
+FROM students
+WHERE github IS NULL
+AND end_date IS NOT NULL;
